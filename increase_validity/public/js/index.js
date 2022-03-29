c313d38eb6b4bd363d6a2683f5db44e97275a92e
akazyti <akazyti@gmail.com>
2022-03-29 12:01:18 +0000

    fix: typo

--- increase_validity/public/js/index.js
+++ increase_validity/public/js/index.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on('Quotation', {
     refresh: async (frm) => {
-        if (!frm.__islocal && frm.doc.stats === "Expired") {
+        if (!frm.__islocal && frm.doc.status === "Expired") {
             frm.add_custom_button('Add Validity', async () => {
                 await frappe.call({
                     "method": "increase_validity.increase",
